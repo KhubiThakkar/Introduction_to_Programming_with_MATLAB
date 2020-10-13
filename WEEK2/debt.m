e48d71a14beb0a77bcab6d007b1d15478c009580
KhubiThakkar <khubithakkar@gmail.com>
2020-10-13 12:42:12 +0530

    Update debt.m

--- WEEK2/debt.m
+++ WEEK2/debt.m
@@ -1,5 +1,5 @@
 %MATLAB AS CALCULATOR
-%We borrowed $1000 at a 10% annual interest raet. If we did not make a
+%We borrowed $1000 at a 10% annual interest rate. If we did not make a
 %payment for two years, and assumming there is no penalty for non payment,
 %how much do we owe now? Assign the result to a vaiable called debt.
 debt = ((1+0.1)^2)*1000
