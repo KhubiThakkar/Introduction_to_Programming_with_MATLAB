40f0cafc654a9f3f755f9fef6e86bc4341c6f71f
KhubiThakkar <khubithakkar@gmail.com>
2020-10-13 12:44:04 +0530

    Update marathon.m

--- WEEK2/marathon.m
+++ WEEK2/marathon.m
@@ -1,8 +1,8 @@
-%1. As of early,Usain Bolt holdsthe world record in the men's 100 meter
+%1. As of early,Usain Bolt holds the world record in the men's 100 meter
 %dash. It is 9.58 seconds. What was his average speed in km/h? Assign the
 %result to a variable called hundred
 %2. kenyan Eliud Kipchoge set a new world record for men of 2:01:39 in
-%September 16,2018. Assign his average speedd in km/h to thw variable
+%September 16,2018. Assign his average speed in km/h to the variable
 %marathon. The marathon distance is 14.192 kilometers.
 km = 100/1000;
 h = 9.58/3600;
